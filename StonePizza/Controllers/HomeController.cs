bbb760f3fa6a04013dd10acb1e937b5d27a33f0a
Kevin Stone <ruca6616@gmail.com>
2020-03-03 16:41:01 -0800

    Created controller and view for menu.

---
+++ StonePizza/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StonePizza.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+    }
+}

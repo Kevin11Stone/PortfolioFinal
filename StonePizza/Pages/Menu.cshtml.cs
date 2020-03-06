2f378ad575e78b3c30bd4c68a4aa84ec6c6651e2
Kevin Stone <ruca6616@gmail.com>
2020-03-06 13:26:52 -0800

    Created razor page to display menu and added to nav bar on layout page.

--- StonePizza/Pages/Menu.cshtml.cs
+++ StonePizza/Pages/Menu.cshtml.cs
@@ -4,21 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
+using StonePizza.Models;
 
 namespace StonePizza.Pages
 {
-    public class PrivacyModel : PageModel
+    public class MenuModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
+        public List<SignaturePizza> SignaturePizzas { get; set; }
+        public List<Beverage> Beverages { get; set; }
+        public List <SideDish> SideDishes { get; set; }
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
 
         public void OnGet()
         {
+
         }
     }
 }
